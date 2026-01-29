2b910aa7c7d04b26a9955fbddf89cea04b68efc5
Duygital <dangtruongduy.digital@gmail.com>
2026-01-29 21:53:24 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -10,7 +10,6 @@ import { Testimonials } from './components/Testimonials';
 import { Contact } from './components/Contact';
 import { FAQ } from './components/FAQ';
 import { Footer } from './components/Footer';
-import { Chatbot } from './components/Chatbot';
 
 const App: React.FC = () => {
   return (
